95892dae714dcafd0e936ff1cddbf358444e9ced
Roland Shoemaker <rolandshoemaker@gmail.com>
2015-03-09 02:44:50 -0700

    started work... ok i just copied the files and replace bash with bourne...

---
+++ src/CodeGen_Bourne.cpp
@@ -0,0 +1,243 @@
+#include <cassert>
+#include "CodeGen_Bourne.h"
+
+using namespace Bish;
+
+void CodeGen_Bourne::indent() {
+    for (unsigned i = 0; i < indent_level; i++) {
+        stream << "    ";
+    }
+}
+
+void CodeGen_Bourne::visit(const Module *n) {
+    for (std::vector<Function *>::const_iterator I = n->functions.begin(),
+             E = n->functions.end(); I != E; ++I) {
+        (*I)->accept(this);
+    }
+    // Insert a call to bish_main().
+    assert(n->main);
+    FunctionCall *call_main = new FunctionCall(n->main->name);
+    visit(call_main);
+    stream << ";\n";
+    delete call_main;
+}
+
+void CodeGen_Bourne::visit(const Block *n) {
+    if (should_print_block_braces()) stream << "{\n";
+    indent_level++;
+    for (std::vector<IRNode *>::const_iterator I = n->nodes.begin(), E = n->nodes.end();
+         I != E; ++I) {
+        indent();
+        (*I)->accept(this);
+        stream << ";\n";
+    }
+    indent_level--;
+    if (should_print_block_braces()) stream << "}\n\n";
+}
+
+void CodeGen_Bourne::visit(const Variable *n) {
+    if (should_quote_variable()) stream << "\"";
+    stream << "$" << lookup_name(n);
+    if (should_quote_variable()) stream << "\"";
+}
+
+void CodeGen_Bourne::visit(const ReturnStatement *n) {
+    stream << "echo ";
+    enable_functioncall_wrap();
+    n->value->accept(this);
+    disable_functioncall_wrap();
+    stream << "; exit";
+}
+
+void CodeGen_Bourne::visit(const IfStatement *n) {
+    stream << "if [[ ";
+    enable_functioncall_wrap();
+    n->pblock->condition->accept(this);
+    disable_functioncall_wrap();
+    stream << " ]]; then\n";
+    disable_block_braces();
+    n->pblock->body->accept(this);
+
+    for (std::vector<PredicatedBlock *>::const_iterator I = n->elses.begin(),
+             E = n->elses.end(); I != E; ++I) {
+        indent();
+        stream << "elif [[ ";
+        enable_functioncall_wrap();
+        (*I)->condition->accept(this);
+        disable_functioncall_wrap();
+        stream << " ]]; then\n";
+        (*I)->body->accept(this);
+    }
+    if (n->elseblock) {
+        indent();
+        stream << "else\n";
+        n->elseblock->accept(this);
+    }
+    
+    enable_block_braces();
+    indent();
+    stream << "fi";
+}
+
+void CodeGen_Bourne::visit(const ForLoop *n) {
+    stream << "for " << lookup_name(n->variable) << " in ";
+    if (n->upper) {
+        stream << "$(seq ";
+        n->lower->accept(this);
+        stream << " ";
+        n->upper->accept(this);
+        stream << ")";
+    } else {
+        disable_quote_variable();
+        n->lower->accept(this);
+        enable_quote_variable();
+    }
+    stream << "; do\n";
+    disable_block_braces();
+    n->body->accept(this);
+    enable_block_braces();
+    indent();
+    stream << "done";
+}
+
+void CodeGen_Bourne::visit(const Function *n) {
+    stream << "function bish_" << n->name << " ";
+    stream << "() ";
+    LetScope *s = new LetScope();
+    push_let_scope(s);
+    // Bourne doesn't allow named arguments to functions.
+    // We have to translate to positional arguments.
+    unsigned i = 1;
+    for (std::vector<Variable *>::const_iterator I = n->args.begin(), E = n->args.end(); I != E; ++I, ++i) {
+      s->add(*I, convert_string(i));
+    }
+    n->body->accept(this);
+    pop_let_scope();
+}
+
+void CodeGen_Bourne::visit(const FunctionCall *n) {
+    const int nargs = n->args.size();
+    if (should_functioncall_wrap()) stream << "$(";
+    stream << "bish_" << n->name;
+    for (int i = 0; i < nargs; i++) {
+        stream << " ";
+        bool old = enable_functioncall_wrap();
+        if (const FunctionCall *FC = dynamic_cast<const FunctionCall*>(n->args[i])) {
+          if (should_quote_variable()) stream << "\"";
+          n->args[i]->accept(this);
+          if (should_quote_variable()) stream << "\"";
+        } else {
+          n->args[i]->accept(this);
+        }
+        set_functioncall_wrap(old);
+    }
+    if (should_functioncall_wrap()) stream << ")";
+}
+
+void CodeGen_Bourne::visit(const ExternCall *n) {
+    if (should_functioncall_wrap()) stream << "$(";
+    disable_quote_variable();
+    for (InterpolatedString::const_iterator I = n->body->begin(), E = n->body->end();
+         I != E; ++I) {
+        if ((*I).is_str()) {
+            stream << (*I).str();
+        } else {
+            assert((*I).is_var());
+            visit((*I).var());
+        }
+    }
+    enable_quote_variable();
+    if (should_functioncall_wrap()) stream << ")";
+}
+
+void CodeGen_Bourne::visit(const Assignment *n) {
+    stream << lookup_name(n->variable) << "=";
+    enable_functioncall_wrap();
+    n->value->accept(this);
+    disable_functioncall_wrap();
+}
+
+void CodeGen_Bourne::visit(const BinOp *n) {
+    bool comparison = false;
+    switch (n->op) {
+    case BinOp::Eq:
+    case BinOp::NotEq:
+    case BinOp::LT:
+    case BinOp::LTE:
+    case BinOp::GT:
+    case BinOp::GTE:
+        comparison = true;
+        break;
+    case BinOp::Add:
+    case BinOp::Sub:
+    case BinOp::Mul:
+    case BinOp::Div:
+        comparison = false;
+        break;
+    }
+        
+    if (!comparison) stream << "$((";
+    disable_quote_variable();
+    n->a->accept(this);
+    switch (n->op) {
+    case BinOp::Eq:
+        stream << " -eq ";
+        break;
+    case BinOp::NotEq:
+        stream << " -ne ";
+        break;
+    case BinOp::LT:
+        stream << " -lt ";
+        break;
+    case BinOp::LTE:
+        stream << " -lte ";
+        break;
+    case BinOp::GT:
+        stream << " -gt ";
+        break;
+    case BinOp::GTE:
+        stream << " -gte ";
+        break;
+    case BinOp::Add:
+        stream << " + ";
+        break;
+    case BinOp::Sub:
+        stream << " - ";
+        break;
+    case BinOp::Mul:
+        stream << " * ";
+        break;
+    case BinOp::Div:
+        stream << " / ";
+        break;
+    }
+    n->b->accept(this);
+    if (!comparison) stream << "))";
+    enable_quote_variable();
+}
+
+void CodeGen_Bourne::visit(const UnaryOp *n) {
+    switch (n->op) {
+    case UnaryOp::Negate:
+        stream << "-";
+        break;
+    }
+    n->a->accept(this);
+}
+
+void CodeGen_Bourne::visit(const Integer *n) {
+    stream << n->value;
+}
+
+void CodeGen_Bourne::visit(const Fractional *n) {
+    stream << n->value;
+}
+
+void CodeGen_Bourne::visit(const String *n) {
+    stream << "\"" << n->value << "\"";
+}
+
+void CodeGen_Bourne::visit(const Boolean *n) {
+    stream << n->value;
+}
+
---
+++ src/CodeGen_Bourne.h
@@ -0,0 +1,109 @@
+#ifndef __BISH_CODEGEN_Bourne_H__
+#define __BISH_CODEGEN_Bourne_H__
+
+#include <stack>
+#include <map>
+#include <iostream>
+#include "IR.h"
+#include "IRVisitor.h"
+
+namespace Bish {
+
+class LetScope {
+public:
+    void add(const Variable *v, const std::string &newname) {
+        rename[v] = newname;
+    }
+
+    bool lookup(const Variable *v, std::string &out) {
+        std::map<const Variable *, std::string>::iterator I = rename.find(v);
+        if (I != rename.end()) {
+            out = I->second;
+            return true;
+        } else {
+            return false;
+        }
+    }
+private:
+    std::map<const Variable *, std::string> rename;
+};
+  
+class CodeGen_Bourne : public IRVisitor {
+public:
+    CodeGen_Bourne(std::ostream &os) : stream(os) {
+        indent_level = 0;
+        block_print_braces = true;
+        functioncall_wrap = false;
+        quote_variable = true;
+    }
+    virtual void visit(const Module *);
+    virtual void visit(const Block *);
+    virtual void visit(const Variable *);
+    virtual void visit(const ReturnStatement *);
+    virtual void visit(const IfStatement *);
+    virtual void visit(const ForLoop *);
+    virtual void visit(const Function *);
+    virtual void visit(const FunctionCall *);
+    virtual void visit(const ExternCall *);
+    virtual void visit(const Assignment *);
+    virtual void visit(const BinOp *);
+    virtual void visit(const UnaryOp *);
+    virtual void visit(const Integer *);
+    virtual void visit(const Fractional *);
+    virtual void visit(const String *);
+    virtual void visit(const Boolean *);
+private:
+    std::stack<LetScope *> let_stack;
+    std::ostream &stream;
+    unsigned indent_level;
+    bool block_print_braces;
+    bool functioncall_wrap;
+    bool quote_variable;
+
+    inline void disable_block_braces() { block_print_braces = false; }
+    inline void enable_block_braces() { block_print_braces = true; }
+    inline bool should_print_block_braces() const { return block_print_braces; }
+    inline void disable_functioncall_wrap() { functioncall_wrap = false; }
+    inline bool enable_functioncall_wrap() { bool v = functioncall_wrap; functioncall_wrap = true; return v; }
+    inline void set_functioncall_wrap(bool v) { functioncall_wrap = v; }
+    inline bool should_functioncall_wrap() const { return functioncall_wrap; }
+    inline void disable_quote_variable() { quote_variable = false; }
+    inline void enable_quote_variable() { quote_variable = true; }
+    inline bool should_quote_variable() const { return quote_variable; }
+
+    void indent();
+    void push_let_scope(LetScope *s) { let_stack.push(s); }
+    LetScope *pop_let_scope() { LetScope *s = let_stack.top(); let_stack.pop(); return s; }
+
+    bool lookup_let(const Variable *v, std::string &out) {
+        std::stack<LetScope *> aux;
+        std::string tmp;
+        bool found = false;
+        while (!let_stack.empty()) {
+            if (let_stack.top()->lookup(v, tmp)) {
+                out = tmp;
+                found = true;
+                break;
+            }
+            aux.push(let_stack.top());
+            let_stack.pop();
+        }
+        while (!aux.empty()) {
+            let_stack.push(aux.top());
+            aux.pop();
+        }
+        return found;
+    }
+
+    std::string lookup_name(const Variable *v) {
+        std::string tmp;
+        if (lookup_let(v, tmp)) {
+            return tmp;
+        } else {
+            return v->name;
+        }
+    }
+};
+
+}
+#endif
